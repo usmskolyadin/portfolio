4de622af2568b4e65de32fdb3e7cb82a197b8fe3
usmskolyadin <koladinmaksim246@gmail.com>
2025-03-31 02:05:28 +0300

    refactor constants

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,55 @@
+export const products = [
+    {
+      id: 1,
+      title: "Web Development",
+      description: "Custom websites and applications.",
+      price: "$1500",
+      images: ["/hero-2.jpg", "/hero-2.jpg", "/service1-3.jpg"],
+      category: "Development",
+    },
+    {
+      id: 2,
+      title: "UI/UX Design",
+      description: "Intuitive and modern user experiences.",
+      price: "$1000",
+      images: ["/hero-2.jpg", "/hero-3.jpg"],
+      category: "Design",
+    },
+    {
+      id: 3,
+      title: "SEO Optimization",
+      description: "Boost your search rankings effectively.",
+      price: "$500",
+      images: ["/hero-2.jpg"],
+      category: "Marketing",
+    },
+  ];
+
+export const features = [
+    {
+      title: "MY DEVELOPER EXP.",
+      description:
+        "Стек технологий подбирается индивидуально под каждый случай, я использую исключительно современные и удобные для обеих сторон технологии. Мой стек: \n\n Бэкенд - Python, FastAPI, Flask, Django Rest Framework/Django, Express.js, Node.js \n\n Фронтенд - JS, React.js/Next.js, Angular.js, SCSS, TailwindCSS \n\n Работа с бд и другие технологии: SQLAlchemy, Aiogram, ",
+      image: "/hero.jpg",
+      image2: "/hero.jpg",
+    },
+    {
+      title: "User-Centered Design",
+      description:
+        "Our designs focus on providing the best experience for users.",
+      image: "/hero-2.jpg",
+      image2: "/hero.jpg",
+    },
+    {
+      title: "Scalable Technology",
+      description:
+        "We develop scalable and efficient software to meet your needs.",
+      image: "/hero-3.jpg",
+      image2: "/hero.jpg",
+    },
+  ];
+  
+export const texts = ["ТВОЕГО", "ВАШЕГО", "НАШЕГО", "ЛЮБОГО"]; // Список слов
+export const typingSpeed = 200; // Скорость печати (мс)
+export const pauseTime = 2500; // Время ожидания перед сменой слова (мс)
+  
