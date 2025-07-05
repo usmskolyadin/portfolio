c8d2cc65fe9be12597c47b76b75a718d277e9e0a
usmskolyadin <koladinmaksim246@gmail.com>
2025-07-06 02:28:04 +0300

    add services

--- src/shared/card-title/CardTitle.tsx
+++ src/shared/card-title/CardTitle.tsx
@@ -1,4 +1,4 @@
 export function CardTitle({ children }: { children: React.ReactNode }) {
-    return <h3 className="text-xl mt-4 font-semibold uppercase">{children}</h3>;
+    return <h3 className="font-benzin lg:text-xl text-md mt-4 font-semibold uppercase">{children}</h3>;
   }
   
