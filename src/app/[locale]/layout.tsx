896a2e3182d74e97edaa7307dcd768bbc417b0af
usmskolyadin <koladinmaksim246@gmail.com>
2026-03-31 12:54:17 +0300

    add loader

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,10 +5,8 @@ import Header from "@/src/widgets/header/Header";
 import Circles from "@/src/shared/circles/Circles";
 import Footer from "@/src/widgets/footer/Footer";
 import { Montserrat, Lato } from 'next/font/google';
-import Head from "./head";
-import ElectricLines from "../shared/electric-lines/ElectricLines";
-import Image from "next/image";
-
+import Head from "../head";
+import ElectricLines from "@/src/shared/electric-lines/ElectricLines";
 
 const montserrat = Montserrat({ 
   subsets: ['latin'],
@@ -24,25 +22,16 @@ export default function RootLayout({ children, params }: { children: React.React
       <Head />
       <body className={`${montserrat.variable} bg-black`}>
         <ReduxProvider>
-          {/* <ElectricLines /> */}
+          <ElectricLines />
           <div className="mx-auto max-w-screen-3xl px-4 sm:px-6 lg:px-8">
             <div className="lg:grid pb-20">
               
-              {children ? children : (
-                      <div className="flex h-screen justify-center items-center bg-black">
-                        <Image
-                          src="/SCH-LOGO.png"
-                          alt="logo"
-                          width={100}
-                          height={100}
-                          priority
-                          className={`w-56 h-32 mr-4 transition-opacity duration-1000`}
-                        />
-                      </div>
-              )}
+              <Header locale={locale}/>
+              {children ? children : <p className="text-white">Контент не загружен</p>}
               <div className="mx-auto w-full">
                 {/* <AudioPlayer /> */}
               </div>
+              <Footer />
             </div>
           </div>
         </ReduxProvider>
